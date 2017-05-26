ad7ca179a5fc5a69cae4787995c4cf2f298a9682
Chris Hinze <chris@Macbook.local>
2017-05-26 15:19:34 +0200

    test build script

--- travis/build.sh
+++ travis/build.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-echo "build noop"
+
+echo "start build"
+
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+	-batchmode \
+	-nographics \
+	-silent-crashes \
+	-logFile $(pwd)/unity.log \
+	-projectpath $(pwd) \
+	-buildOSXUniversalPlayer "$(pwd)/DonUniverso.app" \
+	-quit
+
+cat $(pwd)/unity.log
