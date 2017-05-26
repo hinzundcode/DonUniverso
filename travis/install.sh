0ff103d5888305d040d7ff489c468fb5b7565ba5
Chris Hinze <chris@Macbook.local>
2017-05-26 15:42:49 +0200

    wget instead of curl

--- travis/install.sh
+++ travis/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo 'Downloading Unity 5.6.1f1: '
-curl -o Unity.pkg http://netstorage.unity3d.com/unity/2860b30f0b54/MacEditorInstaller/Unity-5.6.1f1.pkg
+wget -O Unity.pkg http://netstorage.unity3d.com/unity/2860b30f0b54/MacEditorInstaller/Unity-5.6.1f1.pkg
 
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
