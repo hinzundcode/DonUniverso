c0374738356dc2196eba12dc3a14c74fac936b8d
Chris Hinze <chris@Macbook.local>
2017-05-26 14:32:16 +0200

    test other installation method

--- travis/install.sh
+++ travis/install.sh
@@ -1,26 +1,7 @@
 #!/bin/sh
 
-#http://netstorage.unity3d.com/unity/2860b30f0b54/MacEditorInstaller/Unity-5.6.1f1.pkg
+echo 'Downloading Unity 5.6.1f1: '
+curl -o Unity.pkg http://netstorage.unity3d.com/unity/2860b30f0b54/MacEditorInstaller/Unity-5.6.1f1.pkg
 
-BASE_URL=http://download.unity3d.com/download_unity
-HASH=497a0f351392
-VERSION=5.6.1f1
-
-download() {
-	file=$1
-	url="$BASE_URL/$HASH/$package"
-	
-	echo "Download from $url: "
-	curl -o `basename "$package"` "$url"
-}
-
-install() {
-	package=$1
-	download "$package"
-	
-	ls
-	echo "Installing " `basename "$package"`
-	sudo installer -dumplog -package `basename "$package"` -target /
-}
-
-install "MacEditorInstaller/Unity-$VERSION.pkg"
+echo 'Installing Unity.pkg'
+sudo installer -dumplog -package Unity.pkg -target /
